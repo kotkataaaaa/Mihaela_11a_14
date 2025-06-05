e0b792bd51994c9cf64c100e1b4fac61f77ddeec
MihaelaK <mihaelakostova358@gmail.com>
2025-06-05 21:09:24 +0300

    zad8

---
+++ Mihaela_11a_14/Mihaela_11a_14/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Mihaela_11a_14.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Mihaela_11a_14.RestaurantContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Mihaela_11a_14.RestaurantContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
